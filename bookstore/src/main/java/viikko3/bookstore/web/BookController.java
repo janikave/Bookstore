d3fe5a7ce2b7d1068bf7e4d4254f1697901fdbe4
woweezywoo <bhi054@myy.haaga-helia.fi>
2025-02-10 01:44:35 +0200

    tehtävä 3.2

--- bookstore/src/main/java/viikko3/bookstore/web/BookController.java
+++ bookstore/src/main/java/viikko3/bookstore/web/BookController.java
@@ -1,10 +1,14 @@
 package viikko3.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import viikko3.bookstore.domain.BookRepository;
+import viikko3.bookstore.domain.Book;
 
 import org.springframework.ui.Model;
 
@@ -12,11 +16,18 @@ import org.springframework.ui.Model;
 public class BookController {
     @Autowired
 
-    private BookRepository repository;
+    private BookRepository bookRepository;
 
     @GetMapping("/index")
     public String frontPage() {
 
         return "frontpage";
+    }
+
+    @GetMapping("/booklist")
+    public String getBooks(Model model) {
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
     }
 }
